8ef8ad26590a8d789b0fed1e90ee2b58a20259fc
lzt <644904084@qq.com>
2018-09-22 00:42:59 +0800

    修改config

--- app/src/main/java/com/kwunai/github/common/GithubActivity.kt
+++ app/src/main/java/com/kwunai/github/common/GithubActivity.kt
@@ -1,7 +1,6 @@
 package com.kwunai.github.common
 
 import android.databinding.DataBindingUtil
-import android.databinding.DataBindingUtil.setContentView
 import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
